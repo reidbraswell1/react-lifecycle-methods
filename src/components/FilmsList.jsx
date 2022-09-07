5e245523dc146a73413a93054d3bd61653e3a89d
reid <reidbras@gmail.com>
2022-09-06 21:07:29 -0500

    Adding error text paragraph

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -6,7 +6,7 @@ class FilmsList extends Component {
         console.log(`---Begin FilmsList Constructor---`);
         super(props);
 
-        this.state = { list:[] };
+        this.state = { list:[], errorText:"" };
 
         this.getFilms = this.getFilms.bind(this);
 
@@ -21,26 +21,42 @@ class FilmsList extends Component {
 
     getFilms() {
         console.log(`---Begin FilmsList getFilms()---`);
-        const URL = "https://ghibliapi.herokuapp.com/films";
-        let response = fetch(URL)
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data)
-                this.setState({list:data});
+        const BAD_URL = "https://ghibliapi.herokuapp.com/filmss"
+        const GOOD_URL = "https://ghibliapi.herokuapp.com/films"
+        const URL = GOOD_URL;
+        fetch(URL)
+            .then((response) => {
+                if(response.ok) { 
+                    return response.json()
+                }
+                else {
+                    throw new Error("Unknown Network Error Has Occurred");
+                }
             })
-            .catch((err) => console.log(`Error on fetch = ${err}`))
+            .then((data) => {
+                console.log(data);
+                this.setState({list:data, errorText:""});
+            })
+            .catch((err) => { 
+                console.log(`Error on fetch = ${err}`);
+                this.setState( { list: [], errorText: `An error has occurred fetching data from ${URL}: ${err}`});
+            });
         console.log(`---End FilmsList getFilms()---`);
     }
 
     render() {
         console.log(`---Begin FilmsList render()---`);
-        return(<ul className="list-group">
+        return(<div>
+                <ul className="list-group">
                     {this.state.list.map((value,index,array) => {
-                        return(<li className="list-group-item" 
+                        return(
+                                <li className="list-group-item" 
                                     key={value.id} 
                                     id={value.id}>{index+1}. {value.title} - {value.director}
                                 </li>)})}
-            </ul>);
+                </ul>
+                <p className="error"><span className="color-red">{this.state.errorText}</span></p>
+                </div>);
     }
 }
 export default FilmsList
